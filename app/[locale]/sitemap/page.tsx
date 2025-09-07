682f64de6a2cb0fa13c3fefe1fe781016164c7dc
양정운 <yp071704@gmail.com>
2025-09-08 01:35:42 +0900

    feat: footer 에 sitemap 추가

--- app/[locale]/sitemap/page.tsx
+++ app/[locale]/sitemap/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { Link } from '@/i18n/navigation';
 import { Locale, useTranslations } from 'next-intl';
 import { getTranslations } from 'next-intl/server';
@@ -30,12 +31,22 @@ export default function Sitemap() {
   ];
 
   return (
-    <ul>
-      {sitemap.map((site) => (
-        <li key={site.url}>
-          <Link href={site.url}>{site.label}</Link>
-        </li>
-      ))}
-    </ul>
+    <main className="flex justify-center px-4">
+      <div className="w-full max-w-5xl">
+        <ul>
+          {sitemap.map((site) => (
+            <li key={site.url}>
+              <Button
+                variant="link"
+                className="text-blue-500 p-0 text-base"
+                asChild
+              >
+                <Link href={site.url}>{site.label}</Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import '@/styles/globals.css';
 
 type Props = {
   children: React.ReactNode;
